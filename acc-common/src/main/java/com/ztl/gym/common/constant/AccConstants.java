f1b283303fa8ca421ea8c991cfc6e4bc77301d1b
zt_lj <lujian0@chinaztt.com>
2021-04-10 09:30:57 +0800

    常量

--- acc-common/src/main/java/com/ztl/gym/common/constant/AccConstants.java
+++ acc-common/src/main/java/com/ztl/gym/common/constant/AccConstants.java
@@ -1,4 +1,45 @@
 package com.ztl.gym.common.constant;
 
+/**
+ * 业务常量
+ */
 public class AccConstants {
+    /*---------------------------- 用户常量 ----------------------------*/
+    /**
+     * 用户类型-系统
+     */
+    public final static String USER_TYPE_SYSTEM = "00";
+    /**
+     * 用户类型-企业
+     */
+    public final static String USER_TYPE_COMPANY = "10";
+    /**
+     * 用户类型-经销商
+     */
+    public final static String USER_TYPE_TENANT = "20";
+    /**
+     * 用户类型-经销商下级
+     */
+    public final static String USER_TYPE_TENANT_CUSTOM = "30";
+
+    /*---------------------------- 码常量 ----------------------------*/
+    /**
+     * 单码
+     */
+    public final static int CODE_TYPE_SINGLE = 1;
+    /**
+     * 箱码
+     */
+    public final static int CODE_TYPE_BOX = 2;
+
+
+    /*---------------------------- 通用简单状态常量 ----------------------------*/
+    /**
+     * 状态-删除
+     */
+    public final static int STATUS_DELETE = 0;
+    /**
+     * 状态-正常
+     */
+    public final static int STATUS_NORMAL = 1;
 }
