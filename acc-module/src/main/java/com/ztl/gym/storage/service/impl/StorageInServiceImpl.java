d3556099946125f2fbef84250025b7b3a058498c
cai <864601551@qq.com>
2021-06-30 11:11:43 +0800

    优化直接入库接口效率

--- acc-module/src/main/java/com/ztl/gym/storage/service/impl/StorageInServiceImpl.java
+++ acc-module/src/main/java/com/ztl/gym/storage/service/impl/StorageInServiceImpl.java
@@ -106,11 +106,8 @@ public class StorageInServiceImpl implements IStorageInService {
 //        String id = map.get("id").toString();
 //        System.out.println("id=="+id);
         if(map.get("thirdPartyFlag")!=null){
-            List list=(List)map.get("codes");
-            for (int i = 0; i < list.size(); i++) {
-                map.put("code",list.get(i));
-                updateInStatusByCode(map);//PDA端使用
-            }
+            map.put("insFlag","1");
+            updateInStatusByCode(map);//PDA端使用
         }
         return result;
     }
@@ -181,7 +178,16 @@ public class StorageInServiceImpl implements IStorageInService {
         int res = storageInMapper.updateInStatusByCode(map);//更新企业入库信息
         StorageIn storageIn = storageInMapper.selectStorageInById(Long.valueOf(map.get("id").toString()));//查询入库单信息
         String extraNo = storageIn.getExtraNo();//相关单号
-        storageService.addCodeFlow(AccConstants.STORAGE_TYPE_IN, Long.valueOf(map.get("id").toString()), map.get("code").toString());//插入码流转明细，转移到PDA执行
+        if(map.get("insFlag")==null){
+            storageService.addCodeFlow(AccConstants.STORAGE_TYPE_IN, Long.valueOf(map.get("id").toString()), map.get("code").toString());//插入码流转明细，转移到PDA执行
+        }else{
+            List list=(List)map.get("codes");
+            for (int i = 0; i < list.size(); i++) {
+                map.put("code",list.get(i));
+                storageService.addCodeFlow(AccConstants.STORAGE_TYPE_IN, Long.valueOf(map.get("id").toString()), map.get("code").toString());//插入码流转明细，转移到PDA执行
+            }
+        }
+
         if (extraNo != null) {//判断非空
             //判断是否调拨,执行更新调拨单
             if (extraNo.substring(0, 2).equals("DB")) {
