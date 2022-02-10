9f521a1a3fe0a7685bf60893c5fbd8293110becf
zt_csl <864601551@qq.com>
2022-02-10 17:02:26 +0800

    默认图片

--- acc-common/src/main/java/com/ztl/gym/common/constant/AccConstants.java
+++ acc-common/src/main/java/com/ztl/gym/common/constant/AccConstants.java
@@ -216,7 +216,7 @@ public class AccConstants {
     /**
      * 默认图片
      */
-    public final static String DEFAULT_IMAGE = "https://ztl-admin-gongyema-com.obs.cn-east-2.myhuaweicloud.com/acc/image/202108/1425612681095811072.png";
+    public final static String DEFAULT_IMAGE = "https://data-ir.obs.cn-east-2.myhuaweicloud.com/acc/image/202202/1491698876321763328.png";
 
 
 
